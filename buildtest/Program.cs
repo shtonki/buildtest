1e45713f83a26ae07b76ee7ff1a6fa447183e69c
seba <sebastiandeverdier@gmail.com>
2021-01-24 18:28:13 -0500

    testing server capabilities

--- buildtest/Program.cs
+++ buildtest/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace buildtest
 {
@@ -7,6 +10,22 @@ namespace buildtest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            TcpListener serverSocket = new TcpListener(
+                IPAddress.Any, 22);
+            serverSocket.Start();
+
+            Console.WriteLine("ApiRequestListener Started");
+
+            while ((true))
+            {
+                var clientSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine("someone connected");
+            }
+
+            serverSocket.Stop();
+            Console.WriteLine("Exiting...");
+            Console.ReadLine();
         }
     }
 }
